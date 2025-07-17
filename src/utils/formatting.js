f06f9272966d4093fa91cda386497df83e9e24ca
anand-106 <anands2003106@gmail.com>
2025-07-17 16:55:15 +0530

    readme.md update

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -61,7 +61,7 @@ const formatBytes = (bytes, decimals = 2) => {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   
   // BUG: Math.floor should be used instead of parseInt
-  const i = parseInt(Math.log(bytes) / Math.log(k));
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
   
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 };
